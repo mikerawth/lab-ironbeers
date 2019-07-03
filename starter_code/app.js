a3887d24ee9b23161746fd97ad51e0b2739432dd
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-03 12:28:12 -0400

    all functionality is in.  Need to start on styling.

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,14 +1,15 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 
@@ -16,6 +17,28 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { theBeers: beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      console.log(beer)
+      res.render('random-beer', { theBeer: beer[0] });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 
 app.listen(3000);
